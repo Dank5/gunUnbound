9904ec66986a39cb208f4ce28d3b5e97ef380a49
Dank5 <lewis_danks@hotmail.co.uk>
2018-09-21 20:27:47 +0100

    Added game screen and black rectangle

--- game.py
+++ game.py
@@ -1,1 +1,37 @@
-"import pygame" 
+import pygame
+
+def main():
+    pygame.init()
+
+    white = (255,255,255)
+    black = (0,0,0)
+
+    
+    pygame.display.set_caption("hello")
+    screen = pygame.display.set_mode((800,600))
+
+    clock = pygame.time.Clock()
+
+    def mobile(screen, colour, x, y, width, height):
+        pygame.draw.rect(screen, colour, [x,y,width,height])
+    
+    running = True
+    
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+
+            screen.fill(white)
+            mobile(screen, black, 400, 300, 30, 30)
+            pygame.display.update()
+            clock.tick(60)
+            
+            
+    pygame.quit()
+    quit()
+
+
+if __name__=="__main__":
+    main()
